b4cb144cba28ca10e8c3694f5f843475dd7010ea
andy <andy.yang@tijos.net>
2018-11-01 19:19:08 +0800

    增加输入/输出分离引脚接口支持，以适应更多硬件平台。

--- src/main/java/tijos/framework/sensor/dht/TiDHT.java
+++ src/main/java/tijos/framework/sensor/dht/TiDHT.java
@@ -1,4 +1,4 @@
-package tijos.framework.sensor.dht;
+
 
 import java.io.IOException;
 
@@ -12,13 +12,21 @@ import tijos.framework.util.Delay;
 
 public class TiDHT implements ITiEventListener {
 	/**
-	 * TiDHT pin id
+	 * TiDHT pin in id
 	 */
-	private int dhtPin;
+	private int dhtPinI;
+	/**
+	 * TiDHT pin out id
+	 */
+	private int dhtPinO;
 	/**
 	 * TiDHT dht22 flag
 	 */
 	private boolean dht22;
+	/**
+	 * TiDHT I/O mode flag
+	 */
+	private boolean iomode;
 	/**
 	 * TiGPIO object
 	 */
@@ -46,7 +54,8 @@ public class TiDHT implements ITiEventListener {
 	/**
 	 * TiDHT enable flag
 	 */
-	private volatile boolean dhtEnable;
+	private volatile boolean dhtEnable;	
+	
 
 	@Override
 	public TiEventType getType() {
@@ -58,7 +67,7 @@ public class TiDHT implements ITiEventListener {
 		if (!dhtEnable)
 			return;
 		TiGPIOEvent eventObj = (TiGPIOEvent) evt;
-		if (eventObj.getPin() == dhtPin && eventObj.getEvent() == TiGPIO.EVT_FALLINGEDGE) {
+		if (eventObj.getPin() == (iomode ? dhtPinO : dhtPinI) && eventObj.getEvent() == TiGPIO.EVT_FALLINGEDGE) {
 			long time = eventObj.getTime();
 			if (dhtCount <= 0)
 				dhtLastTime = eventObj.getTime();
@@ -68,7 +77,7 @@ public class TiDHT implements ITiEventListener {
 				dhtCount = 0;
 				dhtEnable = false;
 			}
-		}
+		}	
 	}
 
 	/**
@@ -76,12 +85,59 @@ public class TiDHT implements ITiEventListener {
 	 * 
 	 * @param gpio
 	 *            TiGPIO object[IN]
-	 * @param dataPinID
-	 *            data pin id[IN]
+	 * @param ioPinID
+	 *            data I/O pin id[IN]
 	 * @throws IOException
 	 */
-	public TiDHT(TiGPIO gpio, int dataPinID) throws IOException {
-		this(gpio, dataPinID, false);
+	public TiDHT(TiGPIO gpio, int ioPinID) throws IOException {
+		this(gpio, -1, ioPinID, false);
+	}
+	
+	/**
+	 * TiDHT initialization, default:model=DHT11
+	 * 
+	 * @param gpio
+	 *            TiGPIO object[IN]
+	 * @param inPinID
+	 *            data in pin id[IN]
+	 * @param outPinID
+	 *            data out pin id[IN]
+	 * @throws IOException
+	 */
+	public TiDHT(TiGPIO gpio, int inPinID, int outPinID) throws IOException {
+		this(gpio, inPinID, outPinID, false);
+	}
+	
+	/**
+	 * TiDHT initialization
+	 * 
+	 * @param gpio
+	 *            TiGPIO object[IN]
+	 * @param ioPinID
+	 *            data I/O pin id[IN]
+	 * @param model22
+	 *            true:DHT22, false:DHT11
+	 * @throws IOException
+	 */
+	public TiDHT(TiGPIO gpio, int ioPinID, boolean model22) throws IOException {
+		this(gpio, -1, ioPinID, model22 ? 1 : 0);
+	}
+	
+	/**
+	 * TiDHT initialization
+	 * 
+	 * @param gpio
+	 *            TiGPIO object[IN]
+	 * @param inPinID
+	 *            data in pin id[IN]
+	 * @param outPinID
+	 *            data out pin id[IN]
+	 * @param model22
+	 *            true:DHT22, false:DHT11
+	 * @throws IOException
+	 */
+	public TiDHT(TiGPIO gpio, int inPinID, int outPinID, boolean model22) throws IOException {
+		this(gpio, inPinID, outPinID, model22 ? 1 : 0);
 	}
 
 	/**
@@ -89,24 +145,36 @@ public class TiDHT implements ITiEventListener {
 	 * 
 	 * @param gpio
 	 *            TiGPIO object[IN]
-	 * @param dataPinID
-	 *            data pin id[IN]
+	 * @param inPinID
+	 *            data in pin id[IN]
+	 * @param outPinID
+	 *            data out pin id[IN]	 
 	 * @param model22
 	 *            true:DHT22, false:DHT11
 	 * @throws IOException
 	 */
-	public TiDHT(TiGPIO gpio, int dataPinID, boolean model22) throws IOException {
-		gpio.setWorkMode(dataPinID, TiGPIO.IO_FLOATING_OD);
-		gpio.writePin(dataPinID, 1);
-		gpio.setEventParameters(dataPinID, TiGPIO.EVT_FALLINGEDGE, 45);// >=45us
+	private TiDHT(TiGPIO gpio, int inPinID, int outPinID, int model) throws IOException {
+		if(inPinID < 0) {
+			iomode = true;
+		}
+		else {
+			iomode = false;
+		}
+		if(!iomode) {
+			gpio.setWorkMode(inPinID, TiGPIO.INPUT_FLOATING);
+		}
+		gpio.setWorkMode(outPinID, TiGPIO.OUTPUT_OD);
+		gpio.writePin(outPinID, 1);
+		gpio.setEventParameters(iomode ? outPinID : inPinID, TiGPIO.EVT_FALLINGEDGE, 45);// >=45us
 		TiEventService.getInstance().addListener(this);
 		gpioObj = gpio;
-		dhtPin = dataPinID;
+		dhtPinI = inPinID;
+		dhtPinO = outPinID;
 		dhtDelta = new int[43];
 		dhtCount = 0;
 		dhtTemperature = -1;
 		dhtHumidity = -1;
-		dht22 = model22;
+		dht22 = (model > 0) ? true : false;
 		dhtEnable = false;
 	}
 
@@ -122,9 +190,9 @@ public class TiDHT implements ITiEventListener {
 		synchronized (this) {
 			dhtCount = 0;
 			dhtEnable = true;
-			gpioObj.writePin(dhtPin, 0);
+			gpioObj.writePin(dhtPinO, 0);
 			Delay.msDelay(18);
-			gpioObj.writePin(dhtPin, 1);
+			gpioObj.writePin(dhtPinO, 1);
 			for (;;) {
 				if ((int) System.currentTimeMillis() - currTime > 500)
 				{
@@ -139,17 +207,17 @@ public class TiDHT implements ITiEventListener {
 					humidity <<= 1;
 					temperature <<= 1;
 					int t = dhtDelta[3 + i];
-					if (t >= 110)
+					if (t >= 100)
 						humidity |= 1;
 					t = dhtDelta[3 + 16 + i];
-					if (t >= 110)
+					if (t >= 100)
 						temperature |= 1;
 				}
 				int dhtChecksum = 0;
 				for (int i = 0; i < 8; i++) {
 					dhtChecksum <<= 1;
 					int t = dhtDelta[3 + 32 + i];
-					if (t >= 110)
+					if (t >= 100)
 						dhtChecksum |= 1;
 				}
 				int dhtSum = (temperature & 0xff) + (temperature >> 8) + (humidity & 0xff) + (humidity >> 8);
@@ -191,11 +259,29 @@ public class TiDHT implements ITiEventListener {
 	}
 
 	/**
-	 * Gets the data pin id
+	 * Gets the I/O pin id
 	 * 
 	 * @return pin id
 	 */
-	public int getDataPinID() {
-		return dhtPin;
+	public int getIOPinID() {
+		return iomode ? dhtPinO : -1;
+	}
+	
+	/**
+	 * Gets the in pin id
+	 * 
+	 * @return pin id
+	 */
+	public int getInPinID() {
+		return iomode ? -1 : dhtPinI;
+	}
+	
+	/**
+	 * Gets the out pin id
+	 * 
+	 * @return pin id
+	 */
+	public int getOutPinID() {
+		return iomode ? -1 : dhtPinO;
 	}
 }
